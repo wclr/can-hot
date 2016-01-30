4b9032df4c2c0e1b630e74d9890b1e5c8a308e56
Alex <alex.oschepkov@gmail.com>
2016-01-30 22:23:20 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Can-Hot
+
+**REAL** hot reloading for CanJS components without loosing app state!
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+#Licence
+
+ISC.
---
+++ can-hot.js
@@ -0,0 +1,105 @@
+import can from 'can'
+import $ from 'jquery'
+
+// set hotDeps on template files for hot-reload
+if (typeof System !== 'undefined' && System.loads) {
+  var instantiate = System.instantiate
+  System.instantiate = function (load) {
+    if (load.name && /(stache|mustache|ejs)\/system/.test(load.name)) {
+      load.hotDeps = true
+    }
+    return instantiate.apply(this, arguments)
+  }
+}
+
+var removeContainedElements = (elements) => {
+  var i = 0
+  while (elements.length > i) {
+    let contained = elements.reduce((contains, e) => {
+      return contains || $.contains(e, elements[i])
+    }, false)
+    contained ? elements.splice(i, 1) : i++
+  }
+}
+
+const canHot = {
+  preserveState: false,
+  reloadedTags: [],
+  tagToComponent: [],
+  insertedEvent: true,
+  removedEvent: true,
+  before: function () {
+  },
+  after: function () {
+    var tags = canHot.reloadedTags.reduce((prev, tag) =>
+        prev.concat(can.makeArray($(tag)))
+      , [])
+    removeContainedElements(tags)
+    tags.forEach(tag => tag.forceUpdate())
+    canHot.reloadedTags = []
+  }
+}
+
+var oldSetup = can.Component.setup
+
+var knownTags = {}
+
+can.Component.setup = function (stat, proto) {
+  if (knownTags[proto.tag]) {
+    canHot.reloadedTags.push(proto.tag)
+  } else {
+    knownTags[proto.tag] = true
+  }
+
+  oldSetup.apply(this, arguments)
+  canHot.tagToComponent[proto.tag] = this
+}
+
+var oldPrototypeSetup = can.Component.prototype.setup
+
+can.Component.prototype.setup = function (el, componentTagData) {
+  var self = this
+
+  el.forceUpdate = function () {
+    var $el = can.$(el)
+    var viewModel = $el.viewModel()
+
+    var controls = can.data($el, 'controls')
+    if (!controls) {
+      console.warn('[hot-reload/can] element does not have "controls" data', $el)
+    } else {
+      controls.forEach((control) => {
+        if (control.viewModel === viewModel) {
+          if (canHot.removedEvent) {
+            // we do not trigger removed because it breaks things,
+            // but call "removed" method because if often contains some clean up
+            // can.trigger($el, 'removed')
+            control.removed && control.removed($el, {})
+          }
+          control.destroy()
+        }
+      })
+    }
+
+    $el.html('')
+
+    can.data($el, 'preventDataBindings', false)
+
+    var Component = canHot.tagToComponent[self.tag]
+    // new Component(el, componentTagData)
+    Component.prototype.setup(el, componentTagData)
+
+    if (canHot.insertedEvent) {
+      can.trigger($el, 'inserted')
+    }
+
+    if (canHot.preserveState) {
+      viewModel.each((val, attr) => {
+        $el.viewModel(attr, val)
+      })
+    }
+  }
+  oldPrototypeSetup.apply(this, arguments)
+}
+
+export default canHot
